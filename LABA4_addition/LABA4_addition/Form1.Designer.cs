7d820d884dd1fde944211b24722f5267d8eda085
LoOTD <152487934+LoOTD@users.noreply.github.com>
2024-03-23 09:54:32 +0300

    Laba4
    Addition Threads

---
+++ LABA4_addition/LABA4_addition/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace LABA4_addition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.WMP2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.WMP1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Update = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.WMP2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WMP1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "10:00";
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(193, 4);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(113, 47);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "Стоп";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click_1);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(312, 3);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(113, 49);
+            this.Start.TabIndex = 5;
+            this.Start.Text = "Продолжить";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // WMP2
+            // 
+            this.WMP2.Enabled = true;
+            this.WMP2.Location = new System.Drawing.Point(308, 12);
+            this.WMP2.Name = "WMP2";
+            this.WMP2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WMP2.OcxState")));
+            this.WMP2.Size = new System.Drawing.Size(247, 229);
+            this.WMP2.TabIndex = 7;
+            // 
+            // WMP1
+            // 
+            this.WMP1.Enabled = true;
+            this.WMP1.Location = new System.Drawing.Point(8, 12);
+            this.WMP1.Name = "WMP1";
+            this.WMP1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WMP1.OcxState")));
+            this.WMP1.Size = new System.Drawing.Size(247, 229);
+            this.WMP1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Update);
+            this.panel1.Controls.Add(this.Start);
+            this.panel1.Controls.Add(this.Stop);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(8, 258);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(547, 57);
+            this.panel1.TabIndex = 8;
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(431, 4);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(113, 47);
+            this.Update.TabIndex = 6;
+            this.Update.Text = "Обновить";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(561, 319);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.WMP2);
+            this.Controls.Add(this.WMP1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.WMP2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WMP1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Start;
+        private AxWMPLib.AxWindowsMediaPlayer WMP1;
+        private AxWMPLib.AxWindowsMediaPlayer WMP2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Update;
+    }
+}
+
---
+++ LABA4_addition/LABA4_addition/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using AxWMPLib;
+
+namespace LABA4_addition
+{
+    public partial class Form1 : Form
+    {
+        private Class1 class1Instance;
+
+        private Thread countupThread = null;
+
+        private ManualResetEvent pauseEvent =  new ManualResetEvent(true);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            class1Instance = new Class1(label2, WMP2);
+        }
+
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread countupThread = new Thread(CountUp);
+            countupThread.Start();
+            class1Instance.Media();
+
+            Media();
+        }
+
+        private void CountUp()
+        {
+            for (double i = 0.00; i <= 10.00; i++)
+            {
+                pauseEvent.WaitOne();
+                label1.BeginInvoke((MethodInvoker)(() =>
+                {
+                    label1.Text = "Прошло секунд: " + i;
+                }));
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        private void Stop_Click_1(object sender, EventArgs e)
+        {
+            pauseEvent.Reset();
+            class1Instance.StopCountDown();
+
+            WMP1.Invoke((MethodInvoker)(() => WMP1.Ctlcontrols.pause()));
+            class1Instance.Stop();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            pauseEvent.Set();
+            class1Instance.StartCountDown();
+
+            WMP1.Invoke((MethodInvoker)(() => WMP1.Ctlcontrols.play()));
+            class1Instance.Start();
+        }
+
+        private void Media()
+        {
+            string filePath = "C:\\Users\\lotd8\\OneDrive\\Рабочий стол\\University\\С#\\Learning_Work\\LABA4_addition\\LABA4_addition\\Image\\planting_bomb.mp4";
+            
+            WMP1.URL = filePath;
+            WMP1.Invoke((MethodInvoker)(() => WMP1.Ctlcontrols.play()));
+        }
+
+        private void RestartCountUp()
+        {
+            if (countupThread != null && countupThread.IsAlive)
+            {
+                countupThread.Abort();
+            }
+            countupThread = new Thread(CountUp);
+            countupThread.Start();
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            class1Instance.Media();
+            Media();
+            RestartCountUp();
+        }
+    }
+}
