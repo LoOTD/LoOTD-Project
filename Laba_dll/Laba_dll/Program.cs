11d31cd3dddb2778fc71c6bbfd69ce07cea84dbf
LoOTD <152487934+LoOTD@users.noreply.github.com>
2024-03-25 22:31:10 +0300

    Laba_dll
    dll with calculating

---
+++ Laba_dll/Laba_dll/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Proj;
+
+namespace Laba
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            double result_div = MyLibrary.Divide(3,5);
+            Console.WriteLine(result_div);
+
+            double result_add = MyLibrary.Add(3, 5);
+            Console.WriteLine(result_add);
+
+            double result_sub = MyLibrary.Substract(3, 5);
+            Console.WriteLine(result_sub);
+
+            double result_mult = MyLibrary.Multiply(3, 5);
+            Console.WriteLine(result_mult);
+        }
+    }
+}
---
+++ MyLibrary/MyLibrary/MyLibrary.cs
@@ -0,0 +1,26 @@
+﻿namespace Proj
+{
+    public static class MyLibrary
+    {
+        public static double Add(double firstValue, double secondValue)
+        {
+            return firstValue + secondValue;
+        }
+        public static double Substract(double firstValue, double secondValue)
+        {
+            return firstValue - secondValue;
+        }
+        public static double Multiply(double firstValue, double secondValue)
+        {
+            return firstValue * secondValue;
+        }
+        public static double Divide(double firstValue, double secondValue)
+        {
+            if (firstValue == 0)
+            {
+                return 0;
+            }
+            return firstValue / secondValue;
+        }
+    }
+}
